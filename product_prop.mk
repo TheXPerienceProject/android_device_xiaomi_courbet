56b7e659610821a67bbd2a280d8add107d122771
Jean-Michel Trivi <jmtrivi@google.com>
2017-06-22 19:20:26 -0700

    courbet: media volume: from 15 to 25 steps
    Test: play media, use all 25 vol steps and observe varying attenuation
    Bug: 32195657
    Change-Id: I7ac87df0b2b15de3a7bb87e739bd88ef7b185236

--- product_prop.mk
+++ product_prop.mk
@@ -21,7 +21,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.media_vol_default=10 \
-	ro.config.media_vol_steps=15 \
+	ro.config.media_vol_steps=25 \
 	ro.config.vc_call_vol_default=7 \
 	ro.config.vc_call_vol_steps=11
 
