53a0f5caf6c4aca23e173641a898a2354ca23772
Nauval Rizky <enuma.alrizky@gmail.com>
2020-03-31 15:47:13 +0900

    courbet: parts: Implement dc dimming
    Squash of the following commits:
    - parts: Move dc dimming to Settings > Display
    - parts: Implement dc dimming preference
    - parts: Disable dc dimming if it's not supported
    - parts: Add DC-Dimming QS tile
    - sepolicy: Address DC Dimming denial
    
    Co-authored-by: Arian <arian.kulmer@web.de>
    Change-Id: I048371e23c68eae199b429169866af864cc03daf

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,22 +20,29 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.utils.RefreshRateUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.utils.FileUtils;
 
 import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
-
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     public static final String SHAREDD2TW = "sharadeD2TW";
     private ITouchFeature mTouchFeature;
 
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display/msm_fb_ea_enable";
+
     @Override
     public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
         // Refresh rate
         RefreshRateUtils.setFPS(RefreshRateUtils.getRefreshRate(context));
 
@@ -47,7 +54,11 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         
         // Thermal Profiles
         ThermalUtils.startService(context);
-        
+
+        // DC Dimming
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
+        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
+
         //Micro-Service to restore sata of dt2w on reboot
         SharedPreferences prefs = context.getSharedPreferences(SHAREDD2TW, Context.MODE_PRIVATE);
         try {
