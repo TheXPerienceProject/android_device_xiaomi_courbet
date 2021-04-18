5048925b74369b26c59e0f671d092411cae95357
dianlujitao <dianlujitao@lineageos.org>
2021-04-18 20:32:20 +0530

    courbet: Build vendor.qti.hardware.perf@2.0
    For some reason, any prebuilt ones from pre-R ROMs no longer work on R
    and cause the following error:
      E ANDR-PERF: Unable to link to gPerfHal death notifications!
    
    Change-Id: Ia5de8c300d71b6def223e97ef9d58898c9c11a57

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 PRODUCT_PACKAGES += \
     WifiOverlay
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.0
+
 # Releasetool scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/device_nfc_check.sh:install/bin/device_nfc_check.sh
