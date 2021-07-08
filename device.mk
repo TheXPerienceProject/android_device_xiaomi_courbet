7c9164ab3e7d7f6e17336193568d67d6134687e0
youngherna <hernan.mauricio16@gmail.com>
2021-07-08 20:01:27 +0200

    courbet: init script

--- device.mk
+++ device.mk
@@ -4,20 +4,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Shipping level
 PRODUCT_SHIPPING_API_LEVEL := 30
-
-# Dynamic partitions setup
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # AAPT
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# Dynamic partitions setup
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock
 
 # File systems table
 PRODUCT_COPY_FILES += \
@@ -28,8 +29,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
-# Fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mock
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.courbet.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.courbet.rc
 
