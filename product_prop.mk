f27dc4b7c38e83cc515ee741f7c49ff2da139433
youngherna <hernan.mauricio16@gmail.com>
2021-08-14 02:44:24 +0200

    courbet: props: target dpi from system.prop

--- product_prop.mk
+++ product_prop.mk
@@ -9,10 +9,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.set_touch_timer_ms=200 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
-#DPI
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=392
-
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
 	debug.hwui.renderer=skiavk \
