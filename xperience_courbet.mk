fc039a42522c06e77df41ca6b13357aa12ebb249
daniml3 <danimoral1001@gmail.com>
2021-09-21 15:32:53 +0200

    courbet: set the build fingerprint and description at build time
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I016dbee5e4d40dfc7318be3c9501dad02dd28825

--- xperience_courbet.mk
+++ xperience_courbet.mk
@@ -14,6 +14,7 @@ $(call inherit-product, device/xiaomi/courbet/device.mk)
 # Inherit some common Xperience stuff.
 $(call inherit-product, vendor/xperience/config/common.mk)
 
+# Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
@@ -26,6 +27,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="courbet_global-user 11 RKQ1.200826.002 V12.5.5.0.RKQMIXM release-keys"
+    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/courbet_global/courbet:11/RKQ1.200826.002/V12.5.5.0.RKQMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys
