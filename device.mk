b9753e89f3781cc7eb4bb58df0d6a45cbbcd2d5e
akeleyoyo77 <hernan.mauricio16@gmail.com>
2022-02-24 23:30:19 +0100

    initial cleanup

--- device.mk
+++ device.mk
@@ -49,8 +49,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
-    CutoutRingServiceSweet
+    Snap 
 
 # Device Settings
 PRODUCT_PACKAGES += \
@@ -96,15 +95,12 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-plus
+    $(LOCAL_PATH)/overlay 
 
 # rro_overlays
 PRODUCT_PACKAGES += \
     WifiOverlay \
-    SettingsOverlayRR \
-    SweetinproSettingsProvider \
-    SweetinproWifiOverlay
+    SettingsOverlayRR 
 
 # Overlays -- Override vendor ones
 PRODUCT_PACKAGES += \
