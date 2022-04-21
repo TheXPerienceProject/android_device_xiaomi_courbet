a2f573de70b91e6bbdc0354d84f9d955fe4e52ce
youngherna <hernan.mauricio16@mail.ccom>
2022-04-20 18:31:31 -0700

    courbet: add more kernel flags for LLVM tools

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,7 @@ TARGET_KERNEL_CONFIG := courbet_defconfig
 TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
